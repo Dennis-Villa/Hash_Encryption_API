9e3233a852b5004f1d863c536870a5932003d5b9
Dennis-Villa <dennisvillavicencioq@gmail.com>
2024-10-20 18:14:43 +0100

    Añadida redirección a documentación

--- src/swagger.ts
+++ src/swagger.ts
@@ -22,6 +22,10 @@ const options = {
     },
     servers: [
       {
+        url: 'https://hash-encryption-api.onrender.com',
+        description: 'Production server',
+      },
+      {
         url: 'http://localhost:3000',
         description: 'Development server',
       },
